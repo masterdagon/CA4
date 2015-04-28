4aae3cda27dbda96807286dc201dcde608786736
Muggi <masterdagon@hotmail.com>
2015-04-28 18:07:38 +0200

    restAPITests

--- test/backend-tests/restAPITests.js
+++ test/backend-tests/restAPITests.js
@@ -10,50 +10,46 @@ var User = mongoose.model("User");
 var wiki = mongoose.model("wiki");
 
 describe('REST API for /user', function () {
-  //Start the Server before the TESTS
-  before(function (done) {
-    testServer = app.listen(testPort, function () {
-      console.log("Server is listening on: " + testPort);
-      done();
+    //Start the Server before the TESTS
+    before(function (done) {
+        testServer = app.listen(testPort, function () {
+            console.log("Server is listening on: " + testPort);
+            done();
+        })
+            .on('error', function (err) {
+                console.log(err);
+            });
     })
-    .on('error',function(err){
-        console.log(err);
-      });
-  })
 
-  beforeEach(function(done){
-      wiki.remove({},function(){
-          var array = [{title: "test1",abstract:"test1",categories:["test11","test12"]},{title: "test2",abstract:"test2",categories:["test21","test22"]}];
-          wiki.create(array,function(err){
-              done();
-          })
-      })
+    beforeEach(function (done) {
+        wiki.remove({}, function () {
+            var array = [{title: "test1", abstract: "test1", categories: ["test11", "test12"]}, {
+                title: "test2",
+                abstract: "test2",
+                categories: ["test21", "test22"]
+            }];
+            wiki.create(array, function (err) {
+                done();
+            })
+        })
 
-    //User.remove({}, function ()
-    //{
-    //  var array = [{userName : "Lars", email :"lars@a.dk",pw: "xxx"},{userName : "Henrik", email :"henrik@a.dk",pw: "xxx"}];
-    //  User.create(array,function(err){
-    //    done();
-    //  });
-    //});
-  })
+    })
 
-  after(function(){  //Stop server after the test
-    //Uncomment the line below to completely remove the database, leaving the mongoose instance as before the tests
-    //mongoose.connection.db.dropDatabase();
-    testServer.close();
-  })
+    after(function () {  //Stop server after the test
+        //Uncomment the line below to completely remove the database, leaving the mongoose instance as before the tests
+        //mongoose.connection.db.dropDatabase();
+        testServer.close();
+    })
 
-  //it("Should get 2 users; Lars and Henrik", function (done) {
-  //  http.get("http://localhost:"+testPort+"/api/user",function(res){
-  //    res.setEncoding("utf8");//response data is now a string
-  //    res.on("data",function(chunk){
-  //      var n = JSON.parse(chunk);
-  //      n.length.should.equal(2);
-  //      n[0].userName.should.equal("Lars");
-  //      n[1].userName.should.equal("Henrik");
-  //      done();
-  //    });
-  //  })
-  //});
+    it("Should return a wiki with title, abstract and categories", function (done) {
+        http.get("http://localhost:" + testPort + "/api/getWiki/:test1", function (res) {
+            //res.setEncoding("utf8");//response data is now a string
+            res.on("data", function (chunk) {
+                var n = JSON.parse(chunk);
+                n.title.should.equal("test1");
+                n.abstract.should.equal("test1");
+                done();
+            });
+        })
+    });
 });
