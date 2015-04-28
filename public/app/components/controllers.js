3d41cf12b2b48cb564f4b319589b5f7a1f18fba3
Dennis <Dennisheegaard@gmail.com>
2015-04-28 22:06:10 +0200

    edit controller functions

--- public/app/components/controllers.js
+++ public/app/components/controllers.js
@@ -5,34 +5,43 @@ angular.module('myAppRename.controllers', []).
   .controller('MyCtrl2', function ($scope) {
     // write MyCtrl2 here
   }).controller('viewallCTRL',function($scope,InfoFactory){
-        $scope.getwikiFromTitle = InfoFactory.getwikiFromTitle
-            .succes(function(data){
-            $scope.wikiFromTitle = data;
-        })
-            .error(function(data){
-                $scope.wikiFromTitle = data;
-            })
-        $scope.findwiki = InfoFactory.findWiki
-            .succes(function(data){
-                $scope.wikiTitleAbstract = data;
-            })
-            .error(function(data){
-                $scope.wikiTitleAbstract = data;
-            })
-        $scope.getCategories = InfoFactory.getCategories
-            .succes(function(data){
-                $scope.categories = data;
-            })
-            .error(function(data){
-                $scope.categories = data;
-            })
-        $scope.getWikisWithCategory = InfoFactory.getWikisWithCategory
-            .succes(function(data){
-                $scope.wikisWithCategories = data;
-            })
-            .error(function(data){
-                $scope.wikisWithCategories = data;
-            })
+        $scope.getwikiFromTitle = function(){
+            InfoFactory.getwikiFromTitle
+                .succes(function(data){
+                    $scope.wikiFromTitle = data;
+                })
+                .error(function(data){
+                    $scope.wikiFromTitle = data;
+                })
+        }
+        $scope.findwiki = function(){
+            InfoFactory.findWiki
+                .succes(function(data){
+                    $scope.wikiTitleAbstract = data;
+                })
+                .error(function(data){
+                    $scope.wikiTitleAbstract = data;
+                })
+        }
+
+        $scope.getCategories = function(){
+            InfoFactory.getCategories
+                .succes(function(data){
+                    $scope.categories = data;
+                })
+                .error(function(data){
+                    $scope.categories = data;
+                })
+        }
+        $scope.getWikisWithCategory = function(){
+            InfoFactory.getWikisWithCategory
+                .succes(function(data){
+                    $scope.wikisWithCategories = data;
+                })
+                .error(function(data){
+                    $scope.wikisWithCategories = data;
+                })
+        }
     });
 
 
