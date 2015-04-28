714238c4eec9caf20baebe9be6ff24361fef4936
Dennis <Dennisheegaard@gmail.com>
2015-04-28 15:28:52 +0200

    Rename samt model db opsætning

--- server/model/wiki facade.js
+++ server/model/wiki facade.js
@@ -1,3 +1,31 @@
 /**
  * Created by Dennnis on 28-04-2015.
  */
+
+function getWiki(title,callback){
+
+}
+
+function findWiki(searchString,callback){
+
+}
+
+function getWiki(title,callback){
+
+}
+
+function getCategories(callback){
+
+}
+
+function getWikisWithCategory(category,callback){
+
+}
+
+module.exports = {
+    getWiki: getWiki,
+    findWiki: findWiki,
+    getWiki: getWiki,
+    getCategories: getCategories,
+    getWikisWithCategory: getWikisWithCategory
+}
