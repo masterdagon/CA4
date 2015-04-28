2d91e321beba9896d231e428afe1de5ce6958923
Muggi <masterdagon@hotmail.com>
2015-04-28 17:30:10 +0200

    wiki facade, rest_api

--- server/routes/REST_api.js
+++ server/routes/REST_api.js
@@ -69,7 +69,7 @@ router.get('/getWikisWithCategory/:category',function(req,res){
         return;
     }
     var category = req.params.category;
-    wikifacade.findWiki(category,function(err,data){
+    wikiFacade.findWiki(category,function(err,data){
         if(err){
             res.status(500);
             res.end("Error")
