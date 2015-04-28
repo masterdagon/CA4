8094af9fe68014f61fcfa3e849c213fb55be3e95
Muggi <masterdagon@hotmail.com>
2015-04-28 23:12:18 +0200

    wiki html

--- public/app/view/viewall/wiki.js
+++ public/app/view/viewall/wiki.js
@@ -5,17 +5,17 @@ angular.module('myAppRename.wiki', ['ngRoute'])
     .config(['$routeProvider', function($routeProvider) {
         $routeProvider.when('/wiki/:title', {
             templateUrl: 'app/view/viewall/wiki.html',
-            controller: 'wikiCTRL'
+            controller: 'wikiCTRL2'
         });
     }]).controller('wikiCTRL',function($scope,InfoFactory,$routeParams){
         $scope.title = $routeParams.title;
-        $scope.getwiki = function(title){
-            InfoFactory.getwiki(title)
+        $scope.getwiki = function(){
+            InfoFactory.getwiki($scope.title)
                 .success(function(data){
                 $scope.wiki = data;
             }).error(function(data){
                     $scope.wiki = data;
                 })
         }
-    })
+    });
 
