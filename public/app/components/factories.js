759824e76819a5feaee0d9aa7df20a7355732755
Dennis <Dennisheegaard@gmail.com>
2015-04-28 21:08:00 +0200

    factories

--- public/app/components/factories.js
+++ public/app/components/factories.js
@@ -4,11 +4,24 @@
 
 angular.module('myAppRename.factories', []).
   factory('InfoFactory', function () {
-    var info = "Hello World from a Factory";
-    var getInfo = function getInfo(){
-      return info;
-    }
-    return {
-      getInfo: getInfo
-    }
+        var urlBase = '/api/';
+        var dataFactory = {};
+
+        dataFactory.getwikiFromTitle = function () {
+            return $http.get(urlBase);
+        };
+
+        dataFactory.getCustomer = function (id) {
+            return $http.get(urlBase + '/' + id);
+        };
+
+        dataFactory.insertCustomer = function (cust) {
+            return $http.post(urlBase, cust);
+        };
+
+        dataFactory.updateCustomer = function (cust) {
+            return $http.put(urlBase + '/' + cust.ID, cust)
+        };
+
+        return dataFactory;
   });
