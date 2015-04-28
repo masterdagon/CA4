14ab089c697be9969a77cb35eb317b943d7abbb5
Dennis <Dennisheegaard@gmail.com>
2015-04-28 16:25:32 +0200

    temp getCategories

--- server/model/wiki facade.js
+++ server/model/wiki facade.js
@@ -7,7 +7,15 @@ var db = require('./db');
 var wiki = mongoose.model('wiki');
 
 function getWiki(title,callback){
-    
+    wiki.findOne({title: title}, function (err, wikisite) {
+        if (err) {
+            callback(err)
+        } else {
+            callback(null,wikisite)
+        }
+
+
+    })
 }
 
 function findWiki(searchString,callback){
@@ -15,7 +23,33 @@ function findWiki(searchString,callback){
 }
 
 function getCategories(callback){
+    var categoryList
+    var uniqueList
+    wiki.find({},function (err, wikisites){
+        if (err) {
+            callback(err)
+        } else {
+            for(var i=0;i<wikisites.length;i++){
+                templist = wikisites[i].categories
+                for(var j=0;j<templist.length;j++){
+                    categoryList.push(templist[j])
+                }
 
+            }
+            uniqueList=categoryList.filter(function(cat){
+                var exist=false
+                for(var i=0;i<uniqueList.length;i++){
+                    if(uniqueList[i]==cat){
+                        exist = true
+                    }
+                }
+                    if(!exist){
+                        return cat
+                    }
+            })
+    }
+        return uniqueList
+    })
 }
 
 function getWikisWithCategory(category,callback){
