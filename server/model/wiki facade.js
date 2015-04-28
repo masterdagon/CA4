413ca45579a784bc3b312acd5ec56c63450c9735
Dennis <Dennisheegaard@gmail.com>
2015-04-28 16:33:38 +0200

    temp getCategories

--- server/model/wiki facade.js
+++ server/model/wiki facade.js
@@ -40,33 +40,29 @@ function findWiki(searchString, callback) {
 }
 
 function getCategories(callback){
-    var categoryList
-    var uniqueList
-    wiki.find({},function (err, wikisites){
-        if (err) {
-            callback(err)
-        } else {
-            for(var i=0;i<wikisites.length;i++){
-                templist = wikisites[i].categories
-                for(var j=0;j<templist.length;j++){
-                    categoryList.push(templist[j])
-                }
+    var categoryList =[];
+    var uniqueList = [];
+    var wikisites = all;
 
-            }
-            uniqueList=categoryList.filter(function(cat){
-                var exist=false
-                for(var i=0;i<uniqueList.length;i++){
-                    if(uniqueList[i]==cat){
-                        exist = true
-                    }
-                }
-                    if(!exist){
-                        return cat
-                    }
-            })
+    for(var i=0;i<wikisites.length;i++){
+        templist = wikisites[i].categories
+        for(var j=0;j<templist.length;j++){
+            categoryList.push(templist[j])
+        }
+
     }
-        return uniqueList
+    uniqueList=categoryList.filter(function(cat){
+        var exist=false
+        for(var i=0;i<uniqueList.length;i++){
+            if(uniqueList[i]==cat){
+                exist = true
+            }
+        }
+            if(!exist){
+                return cat
+            }
     })
+        callback(null,uniqueList);
 }
 
 function getWikisWithCategory(category, callback) {
