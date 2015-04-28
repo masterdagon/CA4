efa1e79e5f0a8f5faf0261a07dd276b85036d4c1
Muggi <masterdagon@hotmail.com>
2015-04-28 16:14:22 +0200

    wiki facade

--- server/model/wiki facade.js
+++ server/model/wiki facade.js
@@ -5,25 +5,55 @@
 var mongoose = require('mongoose');
 var db = require('./db');
 var wiki = mongoose.model('wiki');
+var all = [];
 
-function getWiki(title,callback){
+(function(){
+    wiki.find("",function(err,data){
+        if(err){
+
+        }else{
+            all = data;
+        }
+    })
+})();
+
+function getWiki(title, callback) {
     callback
 }
 
-function findWiki(searchString,callback){
+function findWiki(searchString, callback) {
+    wiki.find({title: searchString},function(err,data){
+        if(err){
+            callback(err);
+        }else{
+            callback(null,data);
+        }
+    })
 
 }
 
-function getWiki(title,callback){
+function getCategories(callback) {
 
 }
 
-function getCategories(callback){
-
-}
-
-function getWikisWithCategory(category,callback){
-
+function getWikisWithCategory(category, callback) {
+    //var all = null;
+    var result = [];
+    //wiki.find("",function(err,data){
+    //    if(err){
+    //        callback(err)
+    //    }else{
+    //        all = data;
+    //    }
+    //})
+    for(var i = 0; i<all.length;i++){
+        for(var j = 0;j<all[i].categories.length;j++){
+            if(category == all[i].categories[j]){
+                result.push(all[i].categories[j])
+            }
+        }
+    }
+    callback(null,result);
 }
 
 module.exports = {
