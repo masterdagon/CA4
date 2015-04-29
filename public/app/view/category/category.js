01ee9682f3ebf1886753fa6448d7ffaa5674ce49
Muggi <masterdagon@hotmail.com>
2015-04-29 15:42:39 +0200

    category

--- public/app/view/category/category.js
+++ public/app/view/category/category.js
@@ -2,10 +2,27 @@
 
 angular.module('myAppRename.category', ['ngRoute'])
 
-    .config(['$routeProvider', function($routeProvider) {
+    .config(['$routeProvider', function ($routeProvider) {
         $routeProvider.when('/category', {
             templateUrl: 'app/view/category/category.html',
             controller: 'categoryCTRL'
         });
-    }])
+    }]).controller('categoryCTRL', function ($scope, InfoFactory) {
+        $scope.getCategories = function () {
+            InfoFactory.getCategories()
+                .success(function (data) {
+                    $scope.categories = data;
+                }).error(function(data){
+                    $scope.categories = data;
+                })
+        };
+        $scope.getWikisWithCategory = function(category){
+            InfoFactory.getWikisWithCategory(category)
+                .success(function(data){
+                    $scope.wikis = data;
+                }).error(function(data){
+                    $scope.wikis = data;
+                })
+        }
+    })
 
