e6ec704deecc9e78aca62a0baa516e02d0bef777
Muggi <masterdagon@hotmail.com>
2015-04-30 13:39:35 +0200

    category

--- public/app/view/category/category.js
+++ public/app/view/category/category.js
@@ -6,13 +6,13 @@ angular.module('myAppRename.category', ['ngRoute'])
         $routeProvider.when('/category', {
             templateUrl: 'app/view/category/category.html',
             controller: 'categoryCTRL'
-        }).when('/category/:category',{
-            templateUrl : 'app/view/category/titles.html',
+        }).when('/category/:category', {
+            templateUrl: 'app/view/category/titles.html',
             controller: 'titleCTRL'
         });
     }]).controller('categoryCTRL', function ($scope, InfoFactory) {
-        $scope.categoriesdb=[];
-        $scope.categories=[];
+        $scope.categoriesdb = [];
+        $scope.categories = [];
         $scope.getCategories = function () {
             InfoFactory.getCategories()
                 .success(function (data) {
@@ -20,47 +20,45 @@ angular.module('myAppRename.category', ['ngRoute'])
                         return a.toLowerCase().localeCompare(b.toLowerCase());
                     });
                     $scope.categories = $scope.categoriesdb;
-                    $scope.totalItems=data.length;
-                }).error(function(data){
+                    $scope.totalItems = data.length;
+                }).error(function (data) {
                     $scope.categoriesdb = data;
                 })
         };
 
-        $scope.resetCategories = function(){
-            $scope.categories =  $scope.categoriesdb;
+        $scope.resetCategories = function () {
+            $scope.categories = $scope.categoriesdb;
+            $scope.totalItems = $scope.categories.length;
+            $scope.currentPage = 1;
         };
 
         $scope.getCategoriesWithLetter = function (letter) {
-            //InfoFactory.getCategories()
-            //    .success(function (data) {
-                    $scope.categories = $scope.categoriesdb.filter(function (cat) {
-                        if(cat.charAt(0)==letter){
-                            return cat;
-                        }
-                    });
-                    $scope.totalItems=$scope.categories.length
-        //        }).error(function(data){
-        //            $scope.categories = data;
-        //        })
+            $scope.categories = $scope.categoriesdb.filter(function (cat) {
+                if (cat.charAt(0) == letter) {
+                    return cat;
+                }
+            });
+            $scope.totalItems = $scope.categories.length;
+            $scope.currentPage = 1;
         };
 
         $scope.filteredLinks = [];
-        $scope.maxSize= 10;
+        $scope.maxSize = 10;
         $scope.totalItems = 0;
         $scope.currentPage = 1;
         $scope.numPerPage = 30;
-        $scope.$watch("totalItems + currentPage + numberPerPage",function(){
-            var begin = (($scope.currentPage-1)*$scope.numPerPage),end = begin+$scope.numPerPage;
-            $scope.filteredLinks = $scope.categories.slice(begin,end);
+        $scope.$watch("totalItems + currentPage + numberPerPage", function () {
+            var begin = (($scope.currentPage - 1) * $scope.numPerPage), end = begin + $scope.numPerPage;
+            $scope.filteredLinks = $scope.categories.slice(begin, end);
         })
 
-    }).controller('titleCTRL',function($scope,InfoFactory,$routeParams){
+    }).controller('titleCTRL', function ($scope, InfoFactory, $routeParams) {
         $scope.category = $routeParams.category;
-        $scope.getWikisWithCategory = function(){
+        $scope.getWikisWithCategory = function () {
             InfoFactory.getWikisWithCategory($scope.category)
-                .success(function(data){
+                .success(function (data) {
                     $scope.titles = data;
-                }).error(function(data){
+                }).error(function (data) {
                     $scope.titles = data;
                 })
         }
