7c694824cd93f2aa466562d420ae1ae79e3972d7
Muggi <masterdagon@hotmail.com>
2015-05-03 23:28:10 +0200

    factory tests

--- test/karma-tests/app/components/testFactories.js
+++ test/karma-tests/app/components/testFactories.js
@@ -18,3 +18,40 @@
 //
 //  });
 //});
+
+describe('infoFactory',function(){
+    beforeEach(module('myAppRename.factories'));
+    describe('InfoFactory',function(){
+        var scope,factory,httpBackend;
+        beforeEach(inject(function($httpBackend,$rootScope,InfoFactory){
+            httpBackend = $httpBackend;
+            scope = $rootScope;
+            factory = InfoFactory;
+
+            httpBackend.whenGET("/api/getWiki/test").respond("getWiki");
+            httpBackend.whenGET("/api/findWiki/test").respond("findWiki");
+            httpBackend.whenGET("/api/getCategories").respond("getCategories");
+            httpBackend.whenGET("/api/getWikisWithCategory/test").respond("getWikisWithCategory");
+        }));
+
+        it("getWiki: should return correct value from http request to the rest api through method",function(){
+            factory.getwiki("test")
+                .success(function(wiki){
+                    expect(wiki).toBe("getWiki");
+                });
+            factory.findwiki("test")
+                .success(function(wiki){
+                    expect(wiki).toBe("findWiki");
+                });
+            factory.getCategories("test")
+                .success(function(wiki){
+                    expect(wiki).toBe("getCategories");
+                });
+            factory.getWikisWithCategory("test")
+                .success(function(wiki){
+                    expect(wiki).toBe("getWikisWithCategory");
+                });
+
+        })
+    })
+})
--- test/karma.conf.js
+++ test/karma.conf.js
@@ -12,7 +12,7 @@ module.exports = function(config){
       'public/app/components/*.js',
       'public/app/view*/**/*.js',
       'test/karma-tests/app/Routes/*.js',
-      //'test/karma-tests/app/view*/**/*.js',
+      'test/karma-tests/app/components/testFactories.js',
       //'test/karma-tests/app/category/*.js',
       //'test/karma-tests/app/components/*.js',
     ],
